69d87beb8ebbb6e987472d100a7ea8911b8408fe
Zenny00 <joshuacomfort@gmail.com>
2024-08-02 21:22:55 -0400

    Add prop to button

--- src/ForecastSelector.js
+++ src/ForecastSelector.js
@@ -4,7 +4,15 @@ export default function ForecastSelector(props) {
     <div>
       <div>Select Forecast</div>
       <div>
-        <button className="button" active={active === "first"}>
+        <button className="button" onClick={props.buttonFunction}>
+        1 Day
+        </button>
+        <button className="button" onClick={props.buttonFunction>
+        3 Days
+        </button>
+        <button className="button" onClick={props.buttonFunction>
+        7 Days
+        </button>
       </div>
     </div>
   );
