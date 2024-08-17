453997d64b33af83f375c67abadecb6b12cf44bc
Zenny00 <joshuacomfort@gmail.com>
2024-08-17 09:00:16 -0400

    Clean up component CSS and add documentation to search functions

--- src/search_bar/SearchBar.js
+++ src/search_bar/SearchBar.js
@@ -1,31 +1,17 @@
-import logo from '../search.png';
+import logo from "../search.png";
 import { useState } from "react";
+import "./SearchBar.css";
 
 export default function SearchBar(props) {
   const [query, setQuery] = useState([]);
 
   return (
-    <div style={{
-      padding: "12px",
-      borderRadius: "8px",
-      border: "1px solid #ccc",
-      width: "300px",
-      display: "flex",
-      justifyContent: "start",
-      gap: "0.5em",
-      alignItems: "center",
-      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)"
-    }}>
+    <div className="SearchBar">
       <img src={logo} alt="magnifying glass" height={16} width={16} /> 
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        style={{
-          width: "100%",
-          border: "none",
-          zIndex: "1"
-        }}
       />
     </div>
   );
--- src/search_bar/search_bar.ts
+++ src/search_bar/search_bar.ts
@@ -4,6 +4,14 @@ import usZips from 'us-zips';
  *  File containing utility functions for querying the location search bar  
  */
 
+
+/**
+ * A function to match a user's query against the list of zip codes and return those which match
+ *
+ * @param {string} searchQuery - The input from the user to be checked against the list of zip codes
+ * @returns {array} - an array of the found zip codes
+ *
+ */
 export function fetchZipResults(searchQuery) {
   const zipCodes = [];
   const keys = Object.keys(usZips);
